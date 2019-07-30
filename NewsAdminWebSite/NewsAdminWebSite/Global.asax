dd2216f1d234c3a3bf39ab74507f295562198cfa
Fazlı <36913092+fzlink@users.noreply.github.com>
2019-07-30 14:17:19 +0300

    Project Initialized

---
+++ NewsAdminWebSite/NewsAdminWebSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NewsAdminWebSite.MvcApplication" Language="C#" %>
---
+++ NewsAdminWebSite/NewsAdminWebSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(NewsAdminWebSite.Startup))]
+namespace NewsAdminWebSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
